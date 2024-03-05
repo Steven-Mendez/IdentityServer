2cca56f031f9326ff4d2a52f9d6b16f81a5b043e
Steven Mendez <stevenampaiz@gmail.com>
2024-03-04 21:56:32 -0600

    test: :white_check_mark: Add Test for Add and GetById User

--- IdentityServer.InfrastructureTests/Users/UserRepositoryTests.cs
+++ IdentityServer.InfrastructureTests/Users/UserRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using API.Users.Repository;
 using IdentityServer.Domain.Users.Entities;
+using IdentityServer.Domain.Users.Exceptions;
 using IdentityServer.Infrastructure.Data;
 using IdentityServer.Infrastructure.UnitsOfWork;
 using Microsoft.EntityFrameworkCore;
@@ -76,41 +77,103 @@ public class UserRepositoryTests
     }
 
     [Fact]
-    public async Task GetByIdAsync_ShouldReturnUser()
+    public async Task GetByIdAsync_ExistingUser_ReturnsUser()
     {
         // Arrange
         List<User> users =
         [
-                new User
-                {
-                    Id = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64"),
-                    UserName = "user1",
-                    Email = "user1@user1",
-                    Password = @"P@ssw0rd",
-                    FirstName = "user1",
-                    LastName = "user1",
-                    Avatar = @"https://randomuser.me/api/port/2.jpg",
-                    IsBlocked = false,
-                    CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
-                }
+            new User
+            {
+                Id = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64"),
+                UserName = "user1",
+                Email = "user1@user1.com",
+                Password = @"P@ssw0rd",
+                FirstName = "user1",
+                LastName = "user1",
+                Avatar = @"https://randomuser.me/api/portraits/men/2.jpg",
+                IsBlocked = true,
+                CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+            },
+            new User
+            {
+                Id = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f"),
+                UserName = "admin",
+                Email = "admin@admin.com",
+                Password = @"P@ssw0rd",
+                FirstName = "Admin",
+                LastName = "Admin",
+                Avatar = @"https://randomuser.me/api/portraits/men/1.jpg",
+                IsBlocked = false,
+                CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+            },
         ];
 
         var (unitOfWork, _, context) = await SetupUnitOfWorkWithUsersAsync(users);
 
+        var existingUserId = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f");
+
         // Act
-        var result = await unitOfWork.UserRepository.GetByIdAsync(Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64"));
+        var result = await unitOfWork.UserRepository.GetByIdAsync(existingUserId);
 
         // Assert
-        Assert.Equal("user1", result.UserName);
-        Assert.Equal("user1@user1", result.Email);
+        Assert.NotNull(result);
+        Assert.Equal(existingUserId, result.Id);
+        Assert.Equal("admin", result.UserName);
+        Assert.Equal("admin@admin.com", result.Email);
         Assert.Equal(@"P@ssw0rd", result.Password);
-        Assert.Equal("user1", result.FirstName);
-        Assert.Equal("user1", result.LastName);
-        Assert.Equal(@"https://randomuser.me/api/port/2.jpg", result.Avatar);
+        Assert.Equal("Admin", result.FirstName);
+        Assert.Equal("Admin", result.LastName);
+        Assert.Equal(@"https://randomuser.me/api/portraits/men/1.jpg", result.Avatar);
         Assert.False(result.IsBlocked);
-        Assert.Equal(Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f"), result.CreatedBy);
+        Assert.Equal(existingUserId, result.CreatedBy);
 
         await context.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_UserNotExists_ThrowsUserNotFoundException()
+    {
+        // Arrange
+        List<User> users =
+        [
+            new User
+            {
+                Id = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64"),
+                UserName = "user1",
+                Email = "user1@user1.com",
+                Password = @"P@ssw0rd",
+                FirstName = "user1",
+                LastName = "user1",
+                Avatar = @"https://randomuser.me/api/portraits/men/2.jpg",
+                IsBlocked = true,
+                CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+            },
+            new User
+            {
+                Id = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f"),
+                UserName = "admin",
+                Email = "admin@admin.com",
+                Password = @"P@ssw0rd",
+                FirstName = "Admin",
+                LastName = "Admin",
+                Avatar = @"https://randomuser.me/api/portraits/men/1.jpg",
+                IsBlocked = false,
+                CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+            },
+        ];
+
+        var (unitOfWork, _, context) = await SetupUnitOfWorkWithUsersAsync(users);
+
+        var notExistingUserId = Guid.Parse("a4558254-b42e-4abe-8da4-3a56441a25fb");
+
+        // Act
+
+        var exception = await Record.ExceptionAsync(async () => await unitOfWork.UserRepository.GetByIdAsync(notExistingUserId));
+
+        // Assert
+
+        Assert.IsType<UserNotFoundException>(exception);
+        Assert.Equal($"User with id '{notExistingUserId}' not found.", exception?.Message);
     }
 
     [Fact]
@@ -153,6 +216,118 @@ public class UserRepositoryTests
         Assert.Equal(@"https://randomuser.me/api/port/2.jpg", result.Avatar);
         Assert.False(result.IsBlocked);
         Assert.Equal(Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f"), result.CreatedBy);
+
+        await context.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task AddAsync_ValidUser_ReturnsAddedUser()
+    {
+        // Arrange
+        var userId = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64");
+
+        var user = new User
+        {
+            Id = userId,
+            UserName = "user1",
+            Email = "user1@user1",
+            Password = @"P@ssw0rd",
+            FirstName = "user1",
+            LastName = "user1",
+            Avatar = @"https://randomuser.me/api/port/2.jpg",
+            IsBlocked = false,
+            CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+        };
+
+        var (unitOfWork, _, context) = await SetupUnitOfWorkWithUsersAsync(new List<User>());
+
+        // Act
+        var result = await unitOfWork.UserRepository.AddAsync(user);
+        await unitOfWork.SaveChangesAsync();
+
+        // Assert
+        Assert.Equal(userId, result.Id);
+        Assert.Equal("user1", result.UserName);
+        Assert.Equal("user1@user1", result.Email);
+        Assert.Equal(@"P@ssw0rd", result.Password);
+        Assert.Equal("user1", result.FirstName);
+        Assert.Equal("user1", result.LastName);
+        Assert.Equal(@"https://randomuser.me/api/port/2.jpg", result.Avatar);
+        Assert.False(result.IsBlocked);
+        Assert.Equal(Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f"), result.CreatedBy);
+
+        await context.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task AddAsync_DuplicateEmail_ThrowsEmailAlreadyExistsException()
+    {
+        // Arrange
+        var userId = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64");
+
+        var user = new User
+        {
+            Id = userId,
+            UserName = "user1",
+            Email = "user1@user1",
+            Password = @"P@ssw0rd",
+            FirstName = "user1",
+            LastName = "user1",
+            Avatar = @"https://randomuser.me/api/port/2.jpg",
+            IsBlocked = false,
+            CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+        };
+
+        var (unitOfWork, _, context) = await SetupUnitOfWorkWithUsersAsync([user]);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () => await unitOfWork.UserRepository.AddAsync(user));
+
+        // Assert
+        Assert.IsType<EmailAlreadyExistsException>(exception);
+        Assert.Equal("Email user1@user1 already exists.", exception?.Message);
+
+        await context.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task AddAsync_DuplicateUserName_ThrowsUserNameAlreadyExistsException()
+    {
+        // Arrange
+        var userId = Guid.Parse("aa435ca3-4924-4433-88de-f27758f05a64");
+
+        var user = new User
+        {
+            Id = userId,
+            UserName = "user1",
+            Email = "user1@user1",
+            Password = @"P@ssw0rd",
+            FirstName = "user1",
+            LastName = "user1",
+            Avatar = @"https://randomuser.me/api/port/2.jpg",
+            IsBlocked = false,
+            CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+        };
+
+        var (unitOfWork, _, context) = await SetupUnitOfWorkWithUsersAsync([user]);
+
+        // Act
+        var exception = await Record.ExceptionAsync(async () => await unitOfWork.UserRepository.AddAsync(new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = "user1",
+            Email = "user2@user2",
+            Password = @"P@ssw0rd",
+            FirstName = "user2",
+            LastName = "user2",
+            Avatar = @"https://randomuser.me/api/port/2.jpg",
+            IsBlocked = false,
+            CreatedBy = Guid.Parse("6e7f4c0b-3b45-4a1c-a48d-9e531dd6931f")
+        }));
+
+        // Assert
+        Assert.IsType<UserNameAlreadyExistsException>(exception);
+        Assert.Equal("User with username user1 already exists", exception?.Message);
 
         await context.DisposeAsync();
     }
