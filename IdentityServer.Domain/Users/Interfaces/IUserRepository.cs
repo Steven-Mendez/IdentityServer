75aae1acb1735c8a3d18aa69ca56032f50741617
Steven Mendez <stevenampaiz@gmail.com>
2024-05-06 23:12:56 -0600

    refactor: :fire: Refactor IUserRepository interface to remove unused methods and improve code readability

--- IdentityServer.Domain/Users/Interfaces/IUserRepository.cs
+++ IdentityServer.Domain/Users/Interfaces/IUserRepository.cs
@@ -5,16 +5,5 @@ namespace IdentityServer.Domain.Users.Interfaces;
 
 public interface IUserRepository : IRepository<User>
 {
-    Task<User?> GetByEmailAsync(string email);
-    Task<User?> GetByUserNameAsync(string userName);
-    Task<User?> GetByUserNameOrEmailAsync(string userNameOrEmail);
-    Task<IEnumerable<User>> GetActiveUsersAsync();
-    Task<IEnumerable<User>> GetBlockedUsersAsync();
-    Task<bool> UserExistsAsync(string userNameOrEmail);
-    Task<bool> IsEmailUniqueAsync(string email);
-    Task<bool> IsUserNameUniqueAsync(string userName);
-    Task<bool> AnotherUserExistsWithSameEmailAsync(Guid userId, string email);
-    Task<bool> AnotherUserExistsWithSameUserNameAsync(Guid userId, string userName);
-    Task ToggleBlockStatusAsync(Guid userId, bool blockStatus);
     Task<User> AuthenticateAsync(string userNameOrEmail, string password);
 }
